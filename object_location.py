d49dea11294f94ac52c283612bb413245002b4a2
mh105 <alexhe0218@gmail.com>
2024-10-12 15:29:22 -0700

    ENH: increase logging level to debug

--- object_location.py
+++ object_location.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.2a1),
-    on Thu Oct 10 19:28:03 2024
+    on Sat Oct 12 15:25:31 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -245,7 +245,7 @@ def setupLogging(filename):
         )
     else:
         logFile.setLevel(
-            logging.getLevel('warning')
+            logging.getLevel('debug')
         )
     
     return logFile
@@ -5412,6 +5412,8 @@ def endExperiment(thisExp, win=None):
     win : psychopy.visual.Window
         Window for this experiment.
     """
+    # log the filename of last_app_load.log
+    logging.warn('target_last_app_load_log_file: ' + thisExp.dataFileName + '_last_app_load.log')
     if win is not None:
         # remove autodraw from all current components
         win.clearAutoDraw()
